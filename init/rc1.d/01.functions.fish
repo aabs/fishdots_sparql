1397fcf53bf62adf737da65ca35bc7c7b4bd5ea1
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-05-08 11:59:37 +1000

    fixed up remote q/u

--- init/rc1.d/01.functions.fish
+++ init/rc1.d/01.functions.fish
@@ -11,7 +11,14 @@ define_subcommand_nonevented rdf update rdf_update "<sparql_update> send a sparq
 define_subcommand_nonevented rdf lquery rdf_lquery "<query> run a sparql query using local files"
 define_subcommand_nonevented rdf lupdate rdf_lupdate "<update> run a sparql update using local files"
 define_subcommand_nonevented rdf fmt rdf_set_format "<fmt> change the default results format"
+define_subcommand_nonevented rdf show rdf_show "display configuration information"
 
+function rdf_show
+    echo "RDF plugin configuration info:"
+    echo -e "\tPWD \t $PWD"
+    echo -e "\tEndpoint \t $SPARQL_ENDPOINT"
+    echo -e "\tData Files \t $RDF_DATA_FILE"
+end
 
 function rdf_set_ep -a endpoint -d 'set the endpoint to present queries to'
     set -U SPARQL_ENDPOINT "$endpoint"
@@ -26,16 +33,25 @@ function rdf_clear_df -d 'reset the list of data files to apply local operations
     set -U RDF_DATA_FILE ''
 end
 
-function rdf_query -a sparql_query -d 'send a sparql query to the registered server'
-    echo query: $sparql_query
-    echo to: $SPARQL_ENDPOINT
-    echo "$sparql_query" | $JENA_HOME/bin/rsparql --results=$SPARQL_DEFAULT_RESULT_FORMAT --service=$SPARQL_ENDPOINT --query=-
+function rdf_query -a qf -d 'send a sparql query to the registered server'
+  set q $argv
+  if not test -e $qf
+    echo "$argv" > /tmp/blah.rq
+    set q /tmp/blah.rq
+  end
+  set -l cmd "$JENA_HOME/bin/rsparql --results=$SPARQL_DEFAULT_RESULT_FORMAT --service=$SPARQL_ENDPOINT --query='$q'"
+  eval $cmd
 end
 
-function rdf_update -a sparql_update -d 'send a sparql update to the registered server'
-    echo update: $sparql_update
-    echo to: $SPARQL_ENDPOINT
-    echo "$sparql_update" | $JENA_HOME/bin/rupdate --results=$SPARQL_DEFAULT_RESULT_FORMAT --service=$SPARQL_ENDPOINT --update=-
+function rdf_update -a qf -d 'send a sparql update to the registered server'
+  set q $argv
+  if not test -e $qf
+    echo "$argv" > /tmp/blah.rq
+    set q /tmp/blah.rq
+  end
+  set -l cmd "$JENA_HOME/bin/rupdate --service=$SPARQL_ENDPOINT --update='$q'"
+  eval $cmd
+
 end
 
 function rdf_lquery -a qf -d '<query file> send a sparql query to the registered server'
@@ -49,13 +65,13 @@ function rdf_lquery -a qf -d '<query file> send a sparql query to the registered
 end
 
 function rdf_lupdate -d 'send a sparql update to the registered server'
-  echo update: $sparql_update
-  echo to: $SPARQL_ENDPOINT
-  set -l cmd "$JENA_HOME/bin/update --results=$SPARQL_DEFAULT_RESULT_FORMAT $data_files --update=-"
-  echo cmd: $cmd
-  echo query: (cat /dev/stdin)
-
-  #echo "$sparql_update" | $JENA_HOME/bin/update --results=$SPARQL_DEFAULT_RESULT_FORMAT --service=$SPARQL_ENDPOINT --update=-
+  set q $argv
+  if not test -e $qf
+    echo "$argv" > /tmp/blah.rq
+    set q /tmp/blah.rq
+  end
+  set -l cmd "$JENA_HOME/bin/update --results=$SPARQL_DEFAULT_RESULT_FORMAT $RDF_DATA_FILE --update='$q'"
+  eval $cmd
 end
 
 function rdf_set_format -a fmt -d "change the default result format"
